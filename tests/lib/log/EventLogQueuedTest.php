889e66e3739b0953d392cdf21df62a0a3d26b966
marvin255 <postbox433@gmail.com>
2018-03-19 21:17:23 +0500

    add USER_ID to EventLogQueued

--- tests/lib/log/EventLogQueuedTest.php
+++ tests/lib/log/EventLogQueuedTest.php
@@ -47,75 +47,10 @@ class EventLogQueuedTest extends \marvin255\bxloger\tests\BaseCase
         $this->runTestingFor('debug', LogLevel::DEBUG);
     }
 
-    public function testLog()
-    {
-        $message = 'message_' . mt_rand();
-        $context = [
-            'AUDIT_TYPE_ID' => 'AUDIT_TYPE_ID_' . mt_rand(),
-            'MODULE_ID' => 'MODULE_ID_' . mt_rand(),
-            'SITE_ID' => 'SITE_ID_' . mt_rand(),
-        ];
-        $userAgent = 'USER_AGENT_' . mt_rand();
-        $requestUri = 'REQUEST_URI_' . mt_rand();
-        $remoteAddr = 'REMOTE_ADDR_' . mt_rand();
-
-        $toTest = [
-            'AUDIT_TYPE_ID' => $context['AUDIT_TYPE_ID'],
-            'MODULE_ID' => $context['MODULE_ID'],
-            'SITE_ID' => $context['SITE_ID'],
-            'DESCRIPTION' => $message,
-            'SEVERITY' => strtoupper(LogLevel::INFO),
-            'USER_AGENT' => $userAgent,
-            'REQUEST_URI' => $requestUri,
-            'REMOTE_ADDR' => $remoteAddr,
-        ];
-
-        $request = $this->getMockBuilder('\\Bitrix\\Main\\HttpRequest')
-            ->setMethods(['getUserAgent', 'getRequestUri', 'getRemoteAddress'])
-            ->getMock();
-        $request->method('getUserAgent')->will($this->returnValue($userAgent));
-        $request->method('getRequestUri')->will($this->returnValue($requestUri));
-        $request->method('getRemoteAddress')->will($this->returnValue($remoteAddr));
-
-        $sqlHelper = $this->getMockBuilder('\\Bitrix\\Main\\DB\\SqlHelper')
-            ->setMethods(['prepareInsert'])
-            ->getMock();
-        $sqlHelper->method('prepareInsert')->will($this->returnCallback(function ($table, $fields) {
-            $return = ['', ''];
-            foreach ($fields as $key => $value) {
-                $return[0] .= (empty($return[0]) ? '' : ', ') . "`{$key}`";
-                $return[1] .= (empty($return[1]) ? '' : ', ') . "'{$value}'";
-            }
-
-            return $return;
-        }));
-
-        $connection = $this->getMockBuilder('\\Bitrix\\Main\\DB\\Connection')
-            ->setMethods(['getSqlHelper', 'query'])
-            ->getMock();
-        $connection->method('getSqlHelper')->will($this->returnValue($sqlHelper));
-        $connection->expects($this->once())
-            ->method('query')
-            ->with($this->callback(function ($sql) use ($toTest) {
-                $return = true;
-                foreach ($toTest as $key => $item) {
-                    $return = mb_strpos($sql, $key) !== false
-                        && mb_strpos($sql, $item) !== false;
-                    if (!$return) {
-                        break;
-                    }
-                }
-
-                return $return;
-            }));
-
-        $log = new EventLogQueued($connection, $request);
-        $log->changeQueueUsageStatus(false)->log(LogLevel::INFO, $message, $context);
-    }
-
     public function testLogQueue()
     {
         define('SITE_ID', 'SITE_ID_' . mt_rand());
+        $userId = mt_rand();
         $message1 = 'message_1_' . mt_rand();
         $message2 = 'message_2_' . mt_rand();
         $context = [
@@ -136,6 +71,7 @@ class EventLogQueuedTest extends \marvin255\bxloger\tests\BaseCase
                 'USER_AGENT' => $userAgent,
                 'REQUEST_URI' => $requestUri,
                 'REMOTE_ADDR' => $remoteAddr,
+                'USER_ID' => $userId,
             ],
             [
                 'AUDIT_TYPE_ID' => $context['AUDIT_TYPE_ID'],
@@ -146,8 +82,15 @@ class EventLogQueuedTest extends \marvin255\bxloger\tests\BaseCase
                 'USER_AGENT' => $userAgent,
                 'REQUEST_URI' => $requestUri,
                 'REMOTE_ADDR' => $remoteAddr,
+                'USER_ID' => $userId,
             ],
         ];
+
+        global $USER;
+        $USER = $this->getMockBuilder('\\CUser')
+            ->setMethods(['getId'])
+            ->getMock();
+        $USER->method('getId')->will($this->returnValue($userId));
 
         $request = $this->getMockBuilder('\\Bitrix\\Main\\HttpRequest')
             ->setMethods(['getUserAgent', 'getRequestUri', 'getRemoteAddress'])
