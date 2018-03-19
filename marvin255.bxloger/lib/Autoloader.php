d56df40cef3503d83904048357d0ca7e92a802b4
marvin255 <postbox433@gmail.com>
2018-03-19 11:24:32 +0500

    Create Autoloader.php

---
+++ marvin255.bxloger/lib/Autoloader.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace marvin255\bxhttpclient;
+
+use InvalidArgumentException;
+
+/**
+ * Автозагрузчик для классов модуля.
+ */
+class Autoloader
+{
+    /**
+     * Соответствие между пространством имен и папкой в файловой системе.
+     *
+     * @var array
+     */
+    protected static $psr = null;
+    /**
+     * Флаг о том, что автозагрузчик зарегистрирован.
+     *
+     * @var bool
+     */
+    protected static $splRegister = null;
+
+    /**
+     * @param string $path
+     *
+     * @return bool
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function register($prefix = __NAMESPACE__, $path = __DIR__)
+    {
+        $prefix = trim($prefix, ' \\');
+        if ($prefix === '') {
+            throw new InvalidArgumentException('Empty prefix for autoloader');
+        }
+
+        $path = rtrim(trim($path), '/');
+        if ($path === '') {
+            throw new InvalidArgumentException('Empty path for autoloader');
+        }
+
+        self::$psr[$prefix] = $path;
+
+        if (!self::$splRegister) {
+            self::$splRegister = spl_autoload_register([__CLASS__, 'load'], true, true);
+        }
+
+        return self::$splRegister;
+    }
+
+    /**
+     * @param string $class
+     */
+    public static function load($class)
+    {
+        foreach (self::$psr as $prefix => $path) {
+            $len = strlen($prefix);
+            if (strncmp($prefix, $class, $len) !== 0) {
+                continue;
+            }
+            $relative_class = substr($class, $len);
+            $file = $path . '/' . str_replace('\\', '/', $relative_class) . '.php';
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+    }
+}
