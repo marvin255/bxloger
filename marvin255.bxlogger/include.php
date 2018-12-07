5c93348c7ca610f8cc8b3717fb662ee0bf5658ce
marvin255 <postbox433@gmail.com>
2018-12-07 11:52:50 +0500

    add lazy event call for log initiation

--- marvin255.bxlogger/include.php
+++ marvin255.bxlogger/include.php
@@ -1,35 +1,9 @@
 <?php
 
 use marvin255\bxlogger\Autoloader;
-use marvin255\bxlogger\Log;
-use marvin255\bxlogger\log\EventLog;
-use marvin255\bxlogger\log\QueuedLoggerInterface;
-use Bitrix\Main\Event;
-use Bitrix\Main\EventManager;
 
 require_once __DIR__ . '/lib/Autoloader.php';
 
 //подключаем свой psr совместимый автозагрузчик
 Autoloader::register('\\Psr\\Log', __DIR__ . '/psr_log');
 Autoloader::register('\\marvin255\\bxlogger', __DIR__ . '/lib');
-
-//событие для того, чтобы другие модули могли подключить свой логер
-$event = new Event('marvin255.bxlogger', 'createLogger');
-$event->send();
-
-//если в событии не подключен логер, то инстантим по умолчанию
-if (!$customLogger = $event->getParameter('logger')) {
-    $customLogger = new EventLog;
-}
-
-//если был определн логер с очередью запросов, то нужно записать очереь по событию
-if ($customLogger instanceof QueuedLoggerInterface) {
-    EventManager::getInstance()->addEventHandler(
-        'main',
-        'OnAfterEpilog',
-        ['\\marvin255\\bxlogger\\EventManager', 'onAfterEpilog']
-    );
-}
-
-//задаем логер в контейнер для передачи в приложение
-Log::set($customLogger);
--- marvin255.bxlogger/lib/Log.php
+++ marvin255.bxlogger/lib/Log.php
@@ -2,6 +2,10 @@
 
 namespace marvin255\bxlogger;
 
+use marvin255\bxlogger\log\EventLog;
+use marvin255\bxlogger\log\QueuedLoggerInterface;
+use Bitrix\Main\Event;
+use Bitrix\Main\EventManager;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -16,7 +20,21 @@ class Log
      *
      * @var \Psr\Log\LoggerInterface
      */
-    protected static $logger = null;
+    protected static $logger;
+
+    /**
+     * Возвращает из контейнера объект логера.
+     *
+     * @return \Psr\Log\LoggerInterface
+     */
+    public static function get()
+    {
+        if (self::$logger === null) {
+            self::$logger = self::instant();
+        }
+
+        return self::$logger;
+    }
 
     /**
      * Задает контейнеру объект логера.
@@ -29,12 +47,31 @@ class Log
     }
 
     /**
-     * Возвращает из контейнера объект логера.
+     * Инициирует событие для создания логгера, если событие ничего не вернуло,
+     * то инициирует логгер по умолчанию.
      *
      * @return \Psr\Log\LoggerInterface
      */
-    public static function get()
+    protected static function instant()
     {
-        return self::$logger;
+        //событие для того, чтобы другие модули могли подключить свой логер
+        $event = new Event('marvin255.bxlogger', 'createLogger');
+        $event->send();
+
+        //если в событии не подключен логер, то инстантим по умолчанию
+        if (!$customLogger = $event->getParameter('logger')) {
+            $customLogger = new EventLog;
+        }
+
+        //если был определн логер с очередью запросов, то нужно записать очереь по событию
+        if ($customLogger instanceof QueuedLoggerInterface) {
+            EventManager::getInstance()->addEventHandler(
+                'main',
+                'OnAfterEpilog',
+                ['\\marvin255\\bxlogger\\EventManager', 'onAfterEpilog']
+            );
+        }
+
+        return $customLogger;
     }
 }
